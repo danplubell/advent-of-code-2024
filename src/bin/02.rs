1d4009899dbdb0c908e2f3a8e48156f810e8a78b
Dan Plubell <danplubell@gmail.com>
2024-12-05 08:03:51 -0600

    Update day 2 with reading and creating reports

--- src/bin/02.rs
+++ src/bin/02.rs
@@ -1,6 +1,10 @@
 advent_of_code::solution!(2);
 
 pub fn part_one(input: &str) -> Option<u32> {
+    for line in input.lines() {
+        let mut reports: Vec<_> = line.split_whitespace().into_iter().collect();
+        
+    }
     None
 }
 
@@ -15,7 +19,7 @@ mod tests {
     #[test]
     fn test_part_one() {
         let result = part_one(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(2));
     }
 
     #[test]
