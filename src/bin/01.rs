02b811e70e94b4e689a31dfbc7576124113373b3
Dan Plubell <danplubell@gmail.com>
2024-12-04 19:27:32 -0600

    Update for part 2 of day 1

--- src/bin/01.rs
+++ src/bin/01.rs
@@ -1,3 +1,5 @@
+use std::cmp::{max, min};
+
 advent_of_code::solution!(1);
 
 pub fn part_one(input: &str) -> Option<u32> {
@@ -16,10 +18,39 @@ pub fn part_one(input: &str) -> Option<u32> {
     }
     left.sort_by(|a, b| b.cmp(a));
     right.sort_by(|a, b| b.cmp(a));
-    None
+    let mut t = 0u32;
+    for i in 0..left.len() {
+        t += max(left[i],right[i])-min(left[i],right[i]);
+    }
+    
+    Some(t)
 }
 pub fn part_two(input: &str) -> Option<u32> {
-    None
+    let pairs: Vec<&str> = input.split("\n").collect();
+    let number_pairs: Vec<_> = pairs
+        .iter()
+        .map(|s| s.split("   ").collect::<Vec<_>>())
+        .collect();
+    let mut left: Vec<u32>  = vec![];
+    let mut right: Vec<u32> = vec![];
+    for number_pair in number_pairs.iter().filter(|p| p.len() == 2) {
+        let left_nbr = number_pair[0].parse::<u32>().unwrap();
+        let right_nbr = number_pair[1].parse::<u32>().unwrap();
+        left.push(left_nbr);
+        right.push(right_nbr);
+    }
+    let mut t = 0;
+    for l in left.iter() {
+        let mut c = 0;
+        for r in right.iter() {
+            if(r == l) {
+                c += 1;
+            }
+        }
+        t += l * c
+    }
+
+    Some(t)
 }
 
 #[cfg(test)]
@@ -29,12 +60,12 @@ mod tests {
     #[test]
     fn test_part_one() {
         let result = part_one(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(11));
     }
 
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(31));
     }
 }
