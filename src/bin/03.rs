9e5dc9c329c4546759e7cc1bd6ca5a420d229438
Dan Plubell <Dan.Plubell@coxautoinc.com>
2024-12-06 11:37:19 -0600

    Solved day 3 part 1

--- src/bin/03.rs
+++ src/bin/03.rs
@@ -10,10 +10,9 @@ use std::ops::Add;
 //         re.find_iter("xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))")
 pub fn part_one(input: &str) -> Option<u32> {
     let re = Regex::new(r"mul\((\d+),(\d+)\)").unwrap();
+//    let re = Regex::new(r"mul\([^)]*\)").unwrap();
     let mut numbers: Vec<u32> = vec![];
     for capture in re.find_iter(input) {
-        println!("{:?}", capture);
-        
         let inner_re = Regex::new(r"\((\d+(?:,\d+)*)\)").unwrap();
         for c in inner_re.find_iter(capture.as_str()) {
             let trimmed = c
@@ -23,7 +22,6 @@ pub fn part_one(input: &str) -> Option<u32> {
                 .collect::<Vec<_>>()
                 .into_iter()
                 .fold(1, |x, y| x * y.parse::<u32>().unwrap());
-            println!("{}", trimmed);
             numbers.push(trimmed);
         }
     }
