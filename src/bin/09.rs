b2e9541a6abb2a1f408b7db52ad2339a76521461
Dan Plubell <Dan.Plubell@coxautoinc.com>
2024-12-18 09:25:19 -0600

    update to day 9 part 1 expand paris

--- src/bin/09.rs
+++ src/bin/09.rs
@@ -2,18 +2,30 @@ advent_of_code::solution!(9);
 
 pub fn part_one(input: &str) -> Option<u32> {
     //expand input into blocks and files
-    let buffer = Vec::new()
-    input.lines().for_each(|l|{
+    let mut buffer: Vec<i64> = Vec::new();
+    input.lines().for_each(|l| {
         // get pairs of values
-       for (i,(c1,c2)) in l.chars().zip(l.chars().take(1)).enumerate() {
-           // for each pair expand into the buffer
-           let f_blocks = c1.to_digit(10).unwrap_or(0);
-           let s_blocks = c2.to_digit(10).unwrap_or(0);
-           for e in 0..f_blocks {
-               
-           }
-       }
+        let v: Vec<_> = l
+            .chars()
+            .collect::<Vec<_>>()
+            .chunks(2)
+            .map(|chunk| {
+                match chunk.len() == 2 {
+                    true => (chunk[0], chunk[1]),
+                    false => (chunk[0], 0 as char)
+                }
+                })
+            .collect::<Vec<_>>();
+        for (i, (c1, c2)) in v.iter().enumerate() {
+            println!("{i}, {},{}", c1, c2);
+            // for each pair expand into the buffer
+            let f_blocks = c1.to_digit(10).unwrap_or(0);
+            let s_blocks = c2.to_digit(10).unwrap_or(0);
+            (0..f_blocks).for_each(|_e| buffer.push(i as i64));
+            (0..s_blocks).for_each(|_s| buffer.push(-1));
+        }
     });
+    println!("{:?}", buffer);
     //defragment
     None
 }
@@ -38,3 +50,28 @@ mod tests {
         assert_eq!(result, None);
     }
 }
+
+/*
+fn pair_elements(s: &str) -> Vec<(char, char)> {
+    s.chars()
+        .collect::<Vec<_>>()
+        .chunks(2)
+        .filter_map(|chunk| {
+            if chunk.len() == 2 {
+                Some((chunk[0], chunk[1]))
+            } else {
+                None
+            }
+        })
+        .collect()
+}
+
+fn main() {
+    let s = "123456";
+    let result = pair_elements(s);
+
+    for (a, b) in result {
+        println!("({}, {})", a, b);
+    }
+}
+ */
