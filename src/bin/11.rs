7ce84c0f29c7b0df0e4641dfa0a5b40ee5e31271
Dan Plubell <danplubell@gmail.com>
2024-12-29 09:42:40 -0500

    using refcell

--- src/bin/11.rs
+++ src/bin/11.rs
@@ -1,3 +1,6 @@
+use std::cell::RefCell;
+use std::ops::Deref;
+use std::rc::Rc;
 use rayon::iter::split;
 
 advent_of_code::solution!(11);
@@ -52,8 +55,77 @@ pub fn part_one(input: &str) -> Option<usize> {
     Some(stone_list.len())
 }
 
-pub fn part_two(input: &str) -> Option<u32> {
+struct Node {
+    data: Option<i64>,
+    next: Option<Rc<RefCell<Node>>>,
+}
+impl Node {
+    fn add_next(&mut self, data: i64) -> Option<> {
+        let next_node = Node {
+            data: Some(data),
+            next: None,
+        };
+        self.next = Some(Rc::new(RefCell::new(next_node)));
+        self.next.clone()
+    }
+}
+pub fn part_two(input: &str) -> Option<usize> {
+    
+    let list: Vec<_> = input.lines().nth(0)?.split(" ").collect();
+    let base_node = Node {
+        data: None,
+        next: None
+    };
+    let mut current_node = base_node;
+    list.iter().for_each(|n_str|{
+        let n = n_str.parse::<i64>().unwrap();
+        current_node.data = Some(n);
+        let  next_node = Node {
+            data: None,
+            next: None,
+        };
+        let next_node_rc = Rc::new(RefCell::new(next_node));
+        current_node.next = Some(next_node_rc);
+        current_node = next_node;
+    });
     None
+/*    let stones: Vec<i64> = list.iter().map(|s| s.parse::<i64>().unwrap()).collect();
+    let mut stone_list: Vec<i64> = stones.clone();
+    let mut working_list: Vec<i64> = vec![];
+    (0..75).for_each(|i| {
+
+        stone_list.iter().for_each(|n| {
+            let stone_type: StoneType;
+            if *n == 0 {
+                stone_type = StoneType::IsZero;
+            } else if is_even_digits(*n) {
+                stone_type = StoneType::IsEvenDigits;
+            } else {
+                stone_type = StoneType::Multiply;
+            }
+            match stone_type {
+                StoneType::IsZero => {
+                    working_list.push(1)
+                }
+                StoneType::IsEvenDigits => {
+                    let result = split_stone(*n);
+                    working_list.push(result.0);
+                    working_list.push(result.1);
+                }
+                StoneType::Multiply => {
+                    working_list.push(*n * 2024)
+                }
+            }
+        });
+        if stone_list == working_list {
+            println!("equal");
+        }
+        stone_list = working_list.clone();
+        working_list = vec![];
+    });
+    Some(stone_list.len())
+    
+ */
 }
 
 #[cfg(test)]
