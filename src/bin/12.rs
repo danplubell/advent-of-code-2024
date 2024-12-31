662dba68e569bb87c52a79df703c173d8e30bfaf
Dan Plubell <danplubell@gmail.com>
2024-12-31 14:19:47 -0500

    Updates

--- src/bin/12.rs
+++ src/bin/12.rs
@@ -1,10 +1,94 @@
-advent_of_code::solution!(12);
+use std::collections::HashMap;
+use serde_json::value::Index;
 
+advent_of_code::solution!(12);
+type SpeciesType = char;
+type RegionId = usize;
+type Row = usize;
+type Col = usize;
+#[derive(Debug, PartialEq, Clone)]
+struct Plant {
+    species: SpeciesType,
+    region: usize,
+    row: usize,
+    col: usize,
+    top: bool,
+    right: bool,
+    bottom: bool,
+    left: bool,
+}
+impl Plant {
+    fn new(species: SpeciesType, region: usize, row: usize, col: usize)-> Self {
+        Self {
+            species,
+            region,
+            row, 
+            col,
+            top: false,
+            right: false,
+            bottom: false,
+            left: false,
+        }
+    }
+}
+fn get_neighbor_region(species:char, row:usize, col:usize, plants:&HashMap<(Row,Col), Plant, >) -> Option<RegionId> {
+    let neighbors:[(i32,i32);4] = [(-1,0), (0,1), (1,0), (0,-1)];
+    neighbors.map(|n|{
+        let r:i32 = (row as i32) + (n.0);
+        let c:i32 = (col as i32) + (n.1);
+        let key_opt = match (r >=0,c>=0)  {
+            (true, true) => Some((r,c)),
+            _=>None
+        };
+        let result:Option<RegionId> = match key_opt {
+            Some(key_parts) => {
+                let key = (key_parts.0 as usize,key_parts.1 as usize );
+                let plant = plants.get(&key);
+                match plant {
+                    Some(p) => {
+                      if p.species == species {
+                          Some(p.region)
+                      }else {
+                          None
+                      }
+                    },
+                    None => None
+                }
+            }
+            _=>None
+        };
+        result
+    });
+    None
+}
 pub fn part_one(input: &str) -> Option<u32> {
+    let mut plants: HashMap<(Row, Col), Plant> = HashMap::new();
+    let mut regions: HashMap<RegionId, Vec<Plant>> = HashMap::new();
+    let mut region_ids = 0..1000;
+    let mut key: RegionId = 0;
+    input.lines().enumerate().for_each(|(row_idx, row)| {
+        key = region_ids.next().unwrap_or(9999999);
+        row.chars().enumerate().for_each(|(col_idx, species)| {
+            let in_region = get_neighbor_region(species, row_idx, col_idx, &plants);
+            let plant = Plant::new(species, 0, row_idx, col_idx);
+            plants.insert((row_idx, col_idx), plant);
+            
+            let _region  =  match regions.get(&key) {
+                Some(r) => r,
+                None => {
+                    regions.insert(key,vec![]);
+                    regions.get(&key).unwrap()
+                }
+            };
+            
+        })
+    });
+    println!("regions: {:?}", regions);
+    println!("plants: {:?}", plants);
     None
 }
 
-pub fn part_two(input: &str) -> Option<u32> {
+pub fn part_two(_input: &str) -> Option<u32> {
     None
 }
 
