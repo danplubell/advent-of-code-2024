262d415224b544e2feca9b033505742cbc8c5ffa
Dan Plubell <danplubell@gmail.com>
2025-01-12 18:55:06 -0600

    Update for day 15

--- src/bin/15.rs
+++ src/bin/15.rs
@@ -1,7 +1,99 @@
 advent_of_code::solution!(15);
-
+use grid::*;
+#[derive(Debug)]
+enum Direction {
+    Left,
+    Right,
+    Up,
+    Down,
+    NoDirection,
+}
+#[derive(Debug, Clone, Copy, PartialEq)]
+struct Position {
+    row: usize,
+    col: usize,
+}
+fn make_move(direction: Direction, robot_location: Position, grid: &mut Grid<char>) -> Position {
+    match direction {
+        Direction::Right => {
+            let current_position = grid.get_mut(robot_location.row, robot_location.col).unwrap();
+            let new_col = robot_location.col.checked_sub(1);
+            // out of bounds?
+            // up against wall?
+            return match new_col {
+                Some(c) => {
+                    let neighbor = grid.get_mut(robot_location.row, c);
+                    match neighbor {
+                        Some('#') => robot_location,
+                        Some('O') => {
+                            robot_location
+                        }
+                        Some(n) => {
+                            // move to new location
+                            // set neighbor to robot
+                            // set old robot to no location
+                            *current_position = '.';
+                            *n = '@';
+                            Position {
+                                row: robot_location.row,
+                                col: c,
+                            }
+                        }
+                        _ => robot_location
+                    }
+                },
+                None => robot_location
+            }
+        }
+        Direction::Left => {}
+        Direction::Up => {}
+        Direction::Down => {}
+        _ =>    {}
+    }
+   robot_location
+}
 pub fn part_one(input: &str) -> Option<u32> {
+    let mut grid: Grid<char> = Grid::new(8, 8);
+    let mut robot_location: Position = Position { row: 0, col: 0 };
+    input.lines().enumerate().for_each(|(row, l)| {
+        if l.starts_with("#") {
+            l.chars().enumerate().for_each(|(col, c)| {
+                let g = grid.get_mut(row, col).unwrap();
+                if c == '@' {
+                    robot_location = Position { row, col }
+                }
+                *g = c;
+            })
+        } else {
+            // process the moves
+            l.chars().enumerate().for_each(|(col, c)| {
+                let g = grid.get(row, col).unwrap();
+                let direction = match g {
+                    '<' => Direction::Left,
+                    '>' => Direction::Right,
+                    '^' => Direction::Up,
+                    'V' => Direction::Down,
+                    _ => Direction::NoDirection,
+                };
+                robot_location = make_move(direction, robot_location, &mut grid);
+            })
+        }
+    });
+    /*
+    for i in 0..8 {
+        println!();
+        for j in 0..8 {
+            let c = grid.get(i,j).unwrap();
+            print!("{}", c);
+        }
+    }
+
+     */
     None
+}
+
+fn move_robot(p0: &char, p1: &mut Grid<char>) {
+    todo!()
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
