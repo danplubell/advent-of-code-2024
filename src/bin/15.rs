74b3a3efd922c0ed09770fadbede797b4be5f3a4
Dan Plubell <Dan.Plubell@coxautoinc.com>
2025-01-16 16:57:12 -0600

    Update day 15

--- src/bin/15.rs
+++ src/bin/15.rs
@@ -14,10 +14,10 @@ struct Position {
     col: usize,
 }
 const NEIGHBOR_OFFSETS: [(i32, i32); 4] = [(-1, 0), (0, 1), (1, 0), (0, -1)];
-const UP:usize = 0;
-const RIGHT:usize = 1;
-const BOTTOM:usize = 2;
-const LEFT:usize = 3;
+const UP: usize = 0;
+const RIGHT: usize = 1;
+const BOTTOM: usize = 2;
+const LEFT: usize = 3;
 
 fn calc_position(offset: (i32, i32), position: Position) -> Option<Position> {
     Option::from(Position {
@@ -32,7 +32,7 @@ fn make_move(direction: Direction, robot_location: Position, grid: &mut Grid<cha
         Direction::Right => NEIGHBOR_OFFSETS[RIGHT],
         Direction::Left => NEIGHBOR_OFFSETS[LEFT],
         Direction::Down => NEIGHBOR_OFFSETS[BOTTOM],
-        Direction::NoDirection => (0,0)
+        Direction::NoDirection => (0, 0),
     };
     let next_robot_position = calc_position(offset, robot_location);
     if let Some(p) = next_robot_position {
@@ -43,8 +43,8 @@ fn make_move(direction: Direction, robot_location: Position, grid: &mut Grid<cha
             //move boxes if possible
             Some('O') => {
                 move_boxes(grid, offset, p);
-                robot_location
-            },
+                make_move(direction, robot_location, grid)
+            }
             _ => {
                 // Now we can do the mutations one at a time, move the robot
                 // set the previous location to empty '.'
@@ -61,70 +61,85 @@ fn make_move(direction: Direction, robot_location: Position, grid: &mut Grid<cha
                     col: p.col,
                 }
             }
-        }
+        };
     }
     robot_location
 }
 
-fn move_boxes(grid: &mut Grid<char>, offset: (i32,i32), position: Position) {
-    let next_robot_position = calc_position(offset,position );
+fn move_boxes(grid: &mut Grid<char>, offset: (i32, i32), position: Position) -> Option<Position> {
+    let next_robot_position = calc_position(offset, position);
     if let Some(p) = next_robot_position {
         let neighbor_value = grid.get(p.row, p.col);
-        match neighbor_value {
-            Some('.')=> {
-                //move box
+        return match neighbor_value {
+            Some('.') => {
+                // Now we can do the mutations one at a time, move the robot
+                // set the previous location to empty '.'
+                if let Some(current) = grid.get_mut(position.row, position.col) {
+                    *current = '.';
+                }
+
+                // set the box to the next location
+                if let Some(next) = grid.get_mut(p.row, p.col) {
+                    *next = 'O';
+                }
+                Some(Position {
+                    row: position.row,
+                    col: position.col,
+                }) // return the position that opened up
             }
             Some('O') => {
                 // check next box
-                move_boxes(grid, offset, position);
-                // check neighbor again
-                let neighbor_value = grid.get(p.row, p.col);
-
+                let opened_position = move_boxes(grid, offset, p);
+                if let Some(current) = opened_position {
+                    return move_boxes(grid, offset, position)
+                }
+                None
             }
-            _=> ()
+            _ => None,
         }
     }
+    None
 }
 /*
-    match direction {
-        Direction::Right => {
-            let new_col = robot_location.col.checked_add(1);
+match direction {
+    Direction::Right => {
+        let new_col = robot_location.col.checked_add(1);
 
-            match new_col {
-                Some(c) => {
-                    // First check if we can move by copying the neighbor value
-                    let neighbor_value = *grid.get(robot_location.row, c).unwrap_or(&'#');
+        match new_col {
+            Some(c) => {
+                // First check if we can move by copying the neighbor value
+                let neighbor_value = *grid.get(robot_location.row, c).unwrap_or(&'#');
 
-                    match neighbor_value {
-                        '#'  => robot_location,
-                        'O' => {
-                            robot_location
-                        },
-                        _ => {
-                            // Now we can do the mutations one at a time
-                            if let Some(current) = grid.get_mut(robot_location.row, robot_location.col) {
-                                *current = '.';
-                            }
-                            if let Some(next) = grid.get_mut(robot_location.row, c) {
-                                *next = '@';
-                            }
-                            Position {
-                                row: robot_location.row,
-                                col: c,
-                            }
+                match neighbor_value {
+                    '#'  => robot_location,
+                    'O' => {
+                        robot_location
+                    },
+                    _ => {
+                        // Now we can do the mutations one at a time
+                        if let Some(current) = grid.get_mut(robot_location.row, robot_location.col) {
+                            *current = '.';
+                        }
+                        if let Some(next) = grid.get_mut(robot_location.row, c) {
+                            *next = '@';
+                        }
+                        Position {
+                            row: robot_location.row,
+                            col: c,
                         }
                     }
-                },
-                None => robot_location
-            }
+                }
+            },
+            None => robot_location
         }
-        Direction::Left => robot_location,
-        Direction::Up => robot_location,
-        Direction::Down => robot_location,
-        Direction::NoDirection => robot_location,
     }
+    Direction::Left => robot_location,
+    Direction::Up => robot_location,
+    Direction::Down => robot_location,
+    Direction::NoDirection => robot_location,
+}
 
-     */
+ */
 
 /*
 fn make_move1(direction: Direction, robot_location: Position, grid: &mut Grid<char>) -> Position {
@@ -170,7 +185,7 @@ fn make_move1(direction: Direction, robot_location: Position, grid: &mut Grid<ch
  */
 pub fn part_one(input: &str) -> Option<u32> {
     let mut grid: Grid<char> = Grid::new(8, 8);
-    
+
     let mut robot_location: Position = Position { row: 0, col: 0 };
     input.lines().enumerate().for_each(|(row, l)| {
         if l.starts_with("#") {
@@ -181,34 +196,13 @@ pub fn part_one(input: &str) -> Option<u32> {
                 }
                 *g = c;
             })
-        } /*else {
-            // process the moves
-            l.chars().enumerate().for_each(|(col, c)| {
-                let g = grid.get(row, col).unwrap();
-                let direction = match g {
-                    '<' => Direction::Left,
-                    '>' => Direction::Right,
-                    '^' => Direction::Up,
-                    'V' => Direction::Down,
-                    _ => Direction::NoDirection,
-                };
-                robot_location = make_move(direction, robot_location, &mut grid);
-                for i in 0..8 {
-                    println!();
-                    for j in 0..8 {
-                        let c = grid.get(i,j).unwrap();
-                        print!("{}", c);
-                    }
-                }
-            })
         }
-        */
     });
-    
+
     for i in 0..8 {
         println!();
         for j in 0..8 {
-            let c = grid.get(i,j).unwrap();
+            let c = grid.get(i, j).unwrap();
             print!("{}", c);
         }
     }
@@ -227,19 +221,16 @@ pub fn part_one(input: &str) -> Option<u32> {
                 for i in 0..8 {
                     println!();
                     for j in 0..8 {
-                        let c = grid.get(i,j).unwrap();
+                        let c = grid.get(i, j).unwrap();
                         print!("{}", c);
                     }
                 }
             })
         }
     });
-        None
+    None
 }
 
-fn move_robot(p0: &char, p1: &mut Grid<char>) {
-    todo!()
-}
 
 pub fn part_two(input: &str) -> Option<u32> {
     None
@@ -259,5 +250,9 @@ mod tests {
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
         assert_eq!(result, None);
+    }
+    #[test]
+    fn test_move_boxes(){
+        let mut grid: Grid<char> = Grid::new(8, 8);
     }
 }
