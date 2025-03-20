3be735c7d182770cfba415ff7c9880130af688b8
Dan Plubell <danplubell@gmail.com>
2025-03-20 08:05:47 -0500

    Update day 21

--- src/bin/21.rs
+++ src/bin/21.rs
@@ -4,6 +4,7 @@ use std::cmp::Ordering;
 use std::collections::{HashMap, HashSet, VecDeque};
 
 advent_of_code::solution!(21);
+type KeyPad = Vec<Vec<Option<char>>>;
 /*
 direction_keys = {
     "^": (0, 1),
@@ -51,72 +52,42 @@ lazy_static! {
 }
 lazy_static! {
     static ref DIR_DELTA: HashMap<char, (i32, i32)> =
-        HashMap::from([('>', (0, 1)), ('v', (1, 0)), ('<', (0, -1)), ('^', (-1, 0))]) ;
+        HashMap::from([('>', (0, 1)), ('v', (1, 0)), ('<', (0, -1)), ('^', (-1, 0))]);
 }
-fn ways(code:&str,keypad: &HashMap<char, (usize, usize)>) -> Vec<&'static str> {
-    let mut parts:Vec<String> = Vec::new();
-    let mut cur_loc = keypad.get(&'A').unwrap();
-    
-    for c in code.chars() {
-        let next_loc = keypad.get(&c).unwrap();
-        let di:isize = next_loc.0 as isize - cur_loc.0 as isize;
-        let dj:isize = next_loc.1 as isize - cur_loc.1 as isize;
-        
-        let mut moves:String = "".to_string();
-        match di.cmp(&0) {
-            Ordering::Greater => {
-                moves.push_str(&"v".repeat(di as usize));
-            }
-            Ordering::Less => {
-                if di < 0 {
-                    moves.push_str(&"^".repeat((-di) as usize));
-                }
-            }
-            _=>()
-        }
-        match dj.cmp(&0) {
-            Ordering::Greater => {
-                moves.push_str(&">".repeat(dj as usize));
-            } 
-            Ordering::Less => {
-                if dj < 0 {
-                    moves.push_str(&"^".repeat((-dj) as usize));
-                }
-            }
-            _=>()
-        }
-        println!("moves: {} {:?}",c, moves);
-        let permutations = moves.chars().permutations(moves.len());
-        let raw_combos: HashSet<String> = permutations.map(|p|p.iter().collect::<String>() + "A").collect();
-
-        println!("{:?}", raw_combos);
-        let mut combos = Vec::new();
-        for combo in raw_combos {
-            let (ci,cj) = cur_loc;
-            let mut good = true;
-            let combo_slice = &combo[..combo.len()-1];
-            for c in combo_slice.chars() {
-                let (di, dj) = DIR_DELTA.get(&c).unwrap();
-                let (ci, cj) = (ci + *di as usize , cj + *dj as usize);
-                if !keypad.values().contains(&(ci, cj)) {
-                    good = false;
-                    break;
-                };
-                if good {
-                    combos.push(combo.clone())
-                }
+lazy_static! {
+    static ref NUM_KEYPAD:KeyPad= {
+        vec![
+            vec![Some('7'), Some('8'), Some('9')],
+            vec![Some('4'), Some('5'), Some('6')],
+            vec![Some('1'), Some('2'), Some('3')],
+            vec![None, Some('0'), Some('A')],
+        ]
+    };
+}
+lazy_static! {
+    static ref DIR_PAD: KeyPad= {
+        vec![
+            vec![None, Some('^'), Some('A')],
+            vec![Some('<'), Some('v'), Some('>')],
+        ]
+    };
+}
+fn compute_pos(keypad: &KeyPad) -> HashMap<char,(usize, usize)> {
+    let mut pos = HashMap::new();
+    for r in 0..keypad.len() {
+        for c in 0..keypad[r].len() {
+            if keypad[r][c].is_some() {
+                pos.insert(keypad[r][c].unwrap(), (r,c));
             }
         }
-        parts.append(&mut combos);
-        cur_loc = next_loc;
     }
-    println!("parts: {:?}", parts);
-    Vec::new()
+    pos
+}
+fn compute_seqs(keypad:KeyPad)  {
+    
 }
 pub fn part_one(input: &str) -> Option<u32> {
     for input_line in input.lines() {
-        let p = ways(input_line.trim(), &NUMERIC_KEYS);
-        println!("{:?}", p);
     }
     None
 }
@@ -141,8 +112,8 @@ mod tests {
         assert_eq!(result, None);
     }
     #[test]
-    fn test_ways() {
-        let r = ways("<A^A>^^AvvvA", &DIRECTION_KEYS);
-
+    fn test_compute_pos() {
+        let r = compute_pos(&NUM_KEYPAD);
+        println!("{:?}", r);
     }
 }
