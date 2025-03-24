33e57d2e19b6a94cfcdfdf108fd788a7f039a6db
Dan Plubell <Dan.Plubell@coxautoinc.com>
2025-03-24 10:26:04 -0500

    Update day 24

--- src/bin/24.rs
+++ src/bin/24.rs
@@ -6,11 +6,10 @@ advent_of_code::solution!(24);
 struct Wire {
     name: String,
     value: Option<i32>,
-    
 }
 impl Wire {
-    fn new(name: String, value:Option<i32>) -> Wire {
-        Wire { name, value  }
+    fn new(name: String, value: Option<i32>) -> Wire {
+        Wire { name, value }
     }
 }
 #[derive(Debug, Clone, PartialEq, Eq, Hash)]
@@ -19,7 +18,7 @@ enum Operator {
     Xor,
     Or,
 }
-fn wire_value(wire:&str) -> Option<i32> {
+fn wire_value(wire: &str) -> Option<i32> {
     Option::from(wire.to_string().parse::<i32>().unwrap())
 }
 #[derive(Debug, Clone, PartialEq, Eq, Hash)]
@@ -30,32 +29,51 @@ struct Gate<'a> {
 }
 impl<'a> Gate<'a> {
     fn new(inputs: (&'a Wire, &'a Wire), output: &'a Wire, operator: Operator) -> Gate<'a> {
-        Gate { inputs, output, operator }
+        Gate {
+            inputs,
+            output,
+            operator,
+        }
     }
 }
 pub fn part_one(input: &str) -> Option<u32> {
     let mut wires = HashMap::new();
     let mut gates = Vec::new();
     for line in input.lines() {
+        if line.is_empty() {
+            continue;
+        }
         if line.contains(':') {
-            let mut parts = line.split(": ").collect::<Vec<_>>();
-            wires.insert(parts[0], Wire::new(parts[0].to_string(),wire_value(parts[1]) ));
+            let parts = line.split(": ").collect::<Vec<_>>();
+            wires.insert(
+                parts[0],
+                Wire::new(parts[0].to_string(), wire_value(parts[1])),
+            );
             continue;
         };
-        if !line.is_empty() {
-            let mut parts = line.split(" ").collect::<Vec<_>>();
-            let in1 = wires.get(parts[0]).unwrap_or(&Wire::new(parts[0].to_string(), None));
-            let op = match parts[1] {
-                "AND" => Operator::And,
-                "OR" => Operator::Or,
-                "Xor" => Operator::Xor,
-                _ => unreachable!(),
-            };
-            let in2 = wires.get(parts[1]).unwrap_or(&Wire::new(parts[1].to_string(), None));
-            let out = wires.get(parts[2]).unwrap_or(&Wire::new(parts[2].to_string(), None));
-            let gate = Gate::new((in1,in2),out,op );
-            gates.push(gate);
+        let parts = line.split(" ").collect::<Vec<_>>();
+        if !wires.contains_key(&parts[0]) {
+            wires.insert(parts[0], Wire::new(parts[0].to_string(), None));
         }
+        let in1 = wires.get_mut(&parts[0]).unwrap();
+        if !wires.contains_key(&parts[0]) {
+            wires.insert(parts[0], Wire::new(parts[0].to_string(), None));
+        }
+        let in2 = wires.get_mut(&parts[2]).unwrap();
+
+        if !wires.contains_key(&parts[4]) {
+            wires.insert(parts[0], Wire::new(parts[4].to_string(), None));
+        }
+        let out = wires.get_mut(&parts[4]).unwrap();
+
+        let op = match parts[1] {
+            "AND" => Operator::And,
+            "OR" => Operator::Or,
+            "Xor" => Operator::Xor,
+            _ => unreachable!(),
+        };
+        let gate = Gate::new((in1, in2), out, op);
+        gates.push(gate);
     }
     None
 }
