a888b557ef4cec5607dccacc31ff358c92c21201
Dan Plubell <danplubell@gmail.com>
2025-03-29 20:25:53 -0500

    finish day 25

--- src/bin/25.rs
+++ src/bin/25.rs
@@ -1,7 +1,62 @@
-advent_of_code::solution!(25);
+use itertools::Itertools;
 
+advent_of_code::solution!(25);
+const KEY: &str = ".....";
+const LOCK: &str = "#####";
 pub fn part_one(input: &str) -> Option<u32> {
-    None
+    let mut keys = Vec::new();
+    let mut locks = Vec::new();
+    for mut chunks in &input.lines().chunks(8) {
+        // is it a lock or a key
+        let mut is_lock = false;
+        let mut lock_part = [0;5];
+        //iterate through chunks
+        for (idx,c) in chunks.into_iter().enumerate() {
+            if c.is_empty() {
+                continue;
+            }
+            if idx == 6 {
+                if is_lock {
+                    locks.push(lock_part);
+                } else {
+                    keys.push(lock_part);
+                }
+                break;
+            }
+            // check for type and skip first row
+            if idx == 0{
+                if c == LOCK {
+                    is_lock = true;
+                }
+                continue;
+            }
+            // iterate through chunk and calculate heights
+            for (j, p) in c.to_string().chars().enumerate() {
+                if p == '#' {
+                    lock_part[j] += 1;
+                }
+            }
+            
+        }
+    }
+    println!("keys: {:?}", keys);
+    println!("locks: {:?}", locks);
+    let mut total  = 0;
+    for l in locks {
+        for k in &keys {
+            let mut ok = true;
+            for i in 0..k.len() {
+                let t = l[i] + k[i];
+                if t > 5 {
+                    ok = false;
+                }
+            }
+            if ok {
+                total += 1;
+            }
+        }
+    }
+    Some(total)
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
